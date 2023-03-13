d9c6b97787fce3c1e9d8b37d6690642810456a17
jaymeco <jayme.anunciacao@gmail.com>
2023-03-13 02:09:57 +0000

    refactor(routes/auth): Adiciona validator para a request da rota de login.

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { authController } from "../controllers/AuthController";
+import loginRequestValidator from "../validators/requests/loginRequest.validator";
 
 const authRouter = Router();
 
-authRouter.post('/login', authController.login);
+authRouter.post('/login', loginRequestValidator, authController.login);
 
 export default authRouter;
