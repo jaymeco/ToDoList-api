7a6bab8dfd1fbc78034ac377303eab32d527b17a
jaymeco <jayme.anunciacao@gmail.com>
2023-03-13 02:13:20 +0000

    refactor(constants/errors): Adiciona variavel para os erros de validação.
    - Adiciona erros relaciondas ao token na variavel de erros comuns.

--- src/constants/errors.ts
+++ src/constants/errors.ts
@@ -1,4 +1,17 @@
 export const errors = {
   AUTH_CREDENTIALS: 'auth:invalid.credentials',
+  TOKEN_NOT_FOUND: 'auth:token.notFound',
+  INVALID_TOKEN: 'auth:invalid.token',
+  EXPIRED_TOKEN: 'auth:expired.token',
   INTERNAL_ERROR: 'server:unexpected.fail',
 }
+
+
+export const validationErrors = {
+  type: 'invalid.type',
+  notEmpty: 'cannot.be.empty',
+  required: 'required.field',
+  date: 'invalid.date',
+  email: 'invalid.email',
+  minLength: 'lessThen.min.length',
+};
