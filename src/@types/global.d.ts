3f02f1ed0475f16e06c2413d2374f147ebae09a0
jaymeco <jayme.anunciacao@gmail.com>
2023-03-18 15:29:06 +0000

    fix(@types/global.d): Corrige tipagem da variavel DB_PORT.

--- src/@types/global.d.ts
+++ src/@types/global.d.ts
@@ -3,7 +3,7 @@ declare global {
     interface ProcessEnv {
       DB_TYPE: 'postgres';
       DB_HOST: string;
-      DB_PORT: string;
+      DB_PORT: number;
       DB_USERNAME: string;
       DB_PASSWORD: string;
       DB_NAME: string;
